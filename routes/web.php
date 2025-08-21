cacda1be0eb1f71b4b9f4979900ce239dc12047d
Fernando <fernando.e.mejia.e@gmail.com>
2025-08-21 12:13:32 -0600

    menu model

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use App\Http\Controllers\TableController;
+use App\Http\Controllers\MenuControllerController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome');
@@ -13,6 +14,7 @@ Route::get('dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::resource('table', TableController::class);
+Route::resource('menu', TableController::class);
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
