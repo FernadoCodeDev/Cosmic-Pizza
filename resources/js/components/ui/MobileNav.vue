364755c0ae38320ae88b49465005740d0aa33e3f
Fernando <fernando.e.mejia.e@gmail.com>
2025-09-18 16:10:24 -0600

    SVG Mobile Nav

---
+++ resources/js/components/ui/MobileNav.vue
@@ -0,0 +1,61 @@
+<script>
+import Open from '@/assets/svg/Open.vue';
+import Close from '@/assets/svg/Close.vue';
+
+export default {
+    components: {
+        Open,
+        Close,
+    },
+    data() {
+        return {
+            menuOpen: false,
+        };
+    },
+    methods: {
+        toggleMenu() {
+            this.menuOpen = !this.menuOpen;
+        },
+    },
+};
+</script>
+
+<template>
+    <div class="relative text-black">
+        <div class="absolute flex flex-col gap-4 md:hidden">
+            <button @click="toggleMenu"
+                class=" p-2 w-16 h-16 object-contain  transition-all duration-700 ease-out bg-transparent">
+                <Open />
+            </button>
+
+        </div>
+
+        <div
+            :class="`fixed  top-0 left-0 w-full backdrop-blur-[4px] bg-white/30  p-2 md:max-w-40 h-full bg-beige-200 transition-transform duration-700 ease-in-out transform z-50 ${menuOpen ? 'translate-x-0' : '-translate-x-full'}`">
+            <div
+                :class="`fixed  top-0 left-0 w-[90%]  p-2 md:max-w-40 h-full bg-beige-50 transition-transform duration-700 ease-in-out transform z-50 ${menuOpen ? 'translate-x-0' : '-translate-x-full'}`">
+                <div class="flex flex-row justify-end p-2 mb-8">
+                    <button @click="toggleMenu" class="text-black p-2 w-16 h-16 object-contain rounded-md">
+                        <close class="" />
+                    </button>
+                </div>
+
+                <h1 class="font-bold text-beige-950 mb-8">Iniciar sesión / Registrarse</h1>
+
+                <nav
+                    class="w-full flex flex-col pga-2 items-start transition-all duration-500 ease-in-out overflow-hidden">
+                    <a href="/" class="p-2 text-base md:text-lg">
+                        Home
+                    </a>
+                    <a href="/" class="p-2 text-base md:text-lg">
+                        Contact
+                    </a>
+                    <a href="/" class="p-2 text-base md:text-lg">
+                        Us
+                    </a>
+                </nav>
+
+            </div>
+        </div>
+    </div>
+</template>
--- resources/js/layouts/Header.vue
+++ resources/js/layouts/Header.vue
@@ -2,29 +2,35 @@
 import Cart from '@/assets/svg/Cart.vue';
 import CosmicPizzaLogo from "../assets/img/CosmicPizzaLogo.webp"
 import Nav from '@/components/ui/Nav.vue';
+import MobileNav from '@/components/ui/MobileNav.vue';
 
 </script>
 
 <template>
     <div class="flex flex-col w-full h-auto bg-beige-200">
 
-        <div class="flex flex-row justify-between  p-4">
+        <div class="relative flex flex-row justify-between  p-4">
+
+            <div class="md:hidden flex">
+                <MobileNav />
+            </div>
 
             <img :src="CosmicPizzaLogo" alt="CosmicPizzaLogo" class="w-auto h-12 col-span-2" />
 
-            <div class="hidden md:block">
-                <Nav />
-            </div>
+            <div class="flex flex-row justify-between gap-4">
 
-            <div class="w-16 h-16 object-contain">
-                <cart />
+                <div class="hidden md:block">
+                    <Nav />
+                </div>
+
+                <div class="w-16 h-16 object-contain">
+                    <cart />
+                </div>
             </div>
 
         </div>
 
-        <div class="md:hidden flex mx-auto">
-            <Nav />
-        </div>
+
 
     </div>
 </template>
